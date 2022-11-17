e0c2458d54095293bdec50501b54ed0ed791443c
Igor marques <igormarquespe@gmail.com>
2022-11-17 14:36:09 -0300

    Project created

---
+++ back-end/src/main/java/com/dsisale/dsisale/DsiSaleApplication.java
@@ -0,0 +1,13 @@
+package com.dsisale.dsisale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsiSaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsiSaleApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/dsisale/dsisale/DsiSaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsisale.dsisale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsiSaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
