190b5128bb3d6c7e9424f9196c24d840803bc03e
Igor marques <igormarquespe@gmail.com>
2022-11-25 14:02:32 -0300

    Home

--- front-web/src/components/Home/index.tsx
+++ front-web/src/components/Home/index.tsx
@@ -1,14 +1,30 @@
 import './style.css'
 
-const Home = ()=> {
+import main from './main.png'
+const Home = () => {
   return (
     <div className="home-container">
-        <div className="home-content">
-            
+      <div className="home-content">
+        <div className="home-actions">
+          <h1 className="home-title">Peça sua pizza <br />
+            que entregamos <br />
+            o mais rápido possível </h1>
+          <h3 className="home-subtitle">
+            A melhor pizzaria delivery da cidade <br />
+            agora no conforto da sua casa! <br />
+            Quem disse que é preciso sair para comer bem?
+          </h3>
+          <a href="#" className="home-btn-order">PEÇA JÀ !!!</a>
         </div>
+        <div className="home-image">
+
+          <img src={main} alt="" className="" />
+        </div>
+
+      </div>
     </div>
-  )  
- 
+  )
+
 
 }
 
