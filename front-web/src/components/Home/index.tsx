21add13c600416d1c3363eb189a78574ba54385f
Igor marques <igormarquespe@gmail.com>
2022-11-25 01:14:21 -0300

    Add assets

---
+++ front-web/src/components/Home/index.tsx
@@ -0,0 +1,15 @@
+import './style.css'
+
+const Home = ()=> {
+  return (
+    <div className="home-container">
+        <div className="home-content">
+            
+        </div>
+    </div>
+  )  
+ 
+
+}
+
+export default Home;
